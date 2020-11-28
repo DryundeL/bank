d6c3a52fc3d296edad5ba4879a7d5201e322f2ad
invisiblecat <invica@list.ru>
2020-11-28 07:59:23 +0300

    база данных
    регистрация и авторизация из базы данных

---
+++ src/sample/DATABASEautorize.java
@@ -0,0 +1,59 @@
+package sample;
+
+import java.sql.*;
+
+public class DATABASEautorize {
+
+    public static String root="root",
+            password="root",
+            url="jdbc:mysql://localhost/test?serverTimezone=Europe/Moscow&useSSL=false";
+    //данные о бд, имя пользователя, пароль, расположение и коррекция бед с часовым поясом
+    public static int check=0, matching=0, CurrentType;
+
+    public static void autorizefromdatabase() {
+
+        try (Connection conn = DriverManager.getConnection(url, root, password)){ //подключаемся к бд
+            Statement statement=conn.createStatement(); //штука, которая обрабатывает запросы sql
+            ResultSet resultSet = statement.executeQuery("SELECT * from users"); //коробка для записи взятых данных из таблицы
+            //executeQuery метод для извлечения данных
+            matching=0;
+            while(resultSet.next()){ //пока в коробке с данными есть данные
+                check=0;
+                while (check==0) { // пока нужные данные не найдены
+                    String CurrentLogin = resultSet.getString(1);
+                    String CurrentPassword = resultSet.getString(2);
+
+
+                    if (CurrentLogin.equals(Menu.userTextFieldAutoriz.getText().trim())&&matching==0) {
+                        //сравниваем логины, если ещё не найден проходящий
+                        if (CurrentPassword.equals(Menu.pwBoxAutoriz.getText().trim())) { //сравниваем пароли
+                            matching = 1; //логин и пароль совпали с записанными
+                            check = 4;
+                            CurrentType = resultSet.getInt(3);
+                        } else {
+                            matching = 2; //пароль не совпал
+                            check = 4;
+                        }
+                    } else {check=4;}//такого логина нет
+                }
+
+            }
+
+
+
+
+        }
+        catch (SQLException throwables) {
+            Menu.DatabaseFail(); //если не получилось подключиться, держим в курсе
+            throwables.printStackTrace();
+        }
+
+        switch (matching) {
+            case 1: Menu.Alert1(); break; //всё верно
+            case 2: Menu.Alert8(); break;  //пароль неверный
+            case 0: Menu.Alert2(); break;  //логина нет
+
+        }
+
+    }
+}
---
+++ src/sample/DATABASEregistration.java
@@ -0,0 +1,72 @@
+package sample;
+
+import java.sql.*;
+
+public class DATABASEregistration {
+
+    public static String NewLogin="", NewPassword="", NewGuyType="", NewUser="", line=""; //тут всё очевидно
+
+    public static void registrationtodatabase() { NewLogin = Menu.userTextField.getText();
+        try (Connection conn = DriverManager.getConnection(DATABASEautorize.url, DATABASEautorize.root, DATABASEautorize.password)) { //подключаемся к бд
+            Statement statement = conn.createStatement(); //штука, которая обрабатывает запросы sql
+            ResultSet resultSet = statement.executeQuery("SELECT * from users"); //коробка с данными из базы
+            int matching=0;
+            while (resultSet.next()) {//пока в коробке есть данные
+                String CurrentLogin = resultSet.getString(1); //берём данные из первой строки первого столбца
+                if (CurrentLogin.equals(NewLogin.trim())) {matching=1;}  //сравниваем с тем, что введено
+            }
+
+            if (matching==0) {
+                if (NewLogin.trim().isEmpty()) {Menu.Alert9();} else { //еси написан пустой логин, выгоняем
+                    if (NewLogin.indexOf(" ")!=-1) {Menu.Alert10();} else { //если написан логин с пробелами, выгоняем
+                        NewPassword=Menu.pwBox.getText(); //читаем пароль
+                        if (NewPassword.trim().equals("")) {Menu.Alert6();} else { //если пустой пароль, выгоняем
+                            if (NewPassword.indexOf(" ")!=-1) {Menu.Alert11();} else {//если пароль с пробелами, выгоняем
+                                if (NewPassword.equals(Menu.pwBox2.getText())==false) {Menu.Alert3();} else {
+                                    NewGuyType = Menu.selection.getId(); //смотрим какая радио кнопка выбрана
+                                    if (NewGuyType == null) {
+                                        Menu.Alert7(); //если никакая, то просим выбрать всё-таки
+                                    } else {
+                                        if (NewGuyType == "phis")
+                                            statement.executeUpdate("INSERT INTO users (login, password, usertype) VALUES ('" + NewLogin + "', '" + NewPassword + "', " + 2 + ")"); //если физ. лицо, то записываем тип 2
+                                        if (NewGuyType == "yur")
+                                            statement.executeUpdate("INSERT INTO users (login, password, usertype) VALUES ('" + NewLogin + "', '" + NewPassword + "', " + 3 + ")"); //если юр. лицо, то записываем тип 3
+                                        Menu.Alert5();
+                                    }
+                                } } } } } } else Menu.Alert4();
+        } catch (
+                SQLException throwables) {
+            Menu.DatabaseFail(); //если не получилось подключиться, держим в курсе
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void registerworker() {
+             NewLogin = Admin.userTextField.getText();
+            try (Connection conn = DriverManager.getConnection(DATABASEautorize.url, DATABASEautorize.root, DATABASEautorize.password)) { //подключаемся к бд
+                Statement statement = conn.createStatement(); //штука, которая обрабатывает запросы sql
+                ResultSet resultSet = statement.executeQuery("SELECT * from users"); //коробка с данными из базы
+                int matching=0;
+                while (resultSet.next()) {//пока в коробке есть данные
+                    String CurrentLogin = resultSet.getString(1); //берём данные из первой строки первого столбца
+                    if (CurrentLogin.equals(NewLogin.trim())) {matching=1;}  //сравниваем с тем, что введено
+                }
+
+                if (matching==0) {
+                    if (NewLogin.trim().isEmpty()) {Menu.Alert9();} else { //еси написан пустой логин, выгоняем
+                        if (NewLogin.indexOf(" ")!=-1) {Menu.Alert10();} else { //если написан логин с пробелами, выгоняем
+                            NewPassword=Admin.pwBoxAd.getText(); //читаем пароль
+                            if (NewPassword.trim().equals("")) {Menu.Alert6();} else { //если пустой пароль, выгоняем
+                                if (NewPassword.indexOf(" ")!=-1) {Menu.Alert11();} else {//если пароль с пробелами, выгоняем
+                                    if (NewPassword.equals(Admin.pwBoxAd2.getText())==false) {Menu.Alert3();} else {
+                                        statement.executeUpdate("INSERT INTO users (login, password, usertype) VALUES ('" + NewLogin + "', '" + NewPassword + "', " + 1 + ")");
+                                        Menu.Alert5();
+                                    }}}}}} else Menu.Alert4();
+
+            } catch (SQLException throwables) {
+                Menu.DatabaseFail(); //если не получилось подключиться, держим в курсе
+                throwables.printStackTrace();
+            }
+
+    }
+}
